c92a38ead6b0e29e31ee96424e487264017261a0
Ashik <ashikpnc@gmail.com>
2022-11-06 18:14:42 +0530

    changes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://momentswebapp.netlify.app",
-    methods: ["GET", "POST"],
+    origin: "*",
   },
 });
 
